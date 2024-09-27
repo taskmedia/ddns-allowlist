6e7c5c65f72b35151745b6f5c3d7d43dda22a2af
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2024-09-27 11:31:42 +0200

    chore: create and clean main function

--- dyndnswhitelist.go
+++ dyndnswhitelist.go
@@ -2,65 +2,37 @@
 package ddnswhitelist
 
 import (
-	"bytes"
 	"context"
-	"errors"
 	"net/http"
-	"text/template"
 )
 
-// Config the plugin configuration.
+// Config the plugin configuration
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
+	DdnsHostList []string `json:"ddnsHostList,omitempty"` // Add hosts to whitelist
 }
 
-// CreateConfig creates the default plugin configuration.
+// CreateConfig creates the default plugin configuration
 func CreateConfig() *Config {
 	return &Config{
-		Headers: make(map[string]string),
+		DdnsHostList: []string{},
 	}
 }
 
-// Demo a Demo plugin.
-type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+// DDNSwhitelist plugin
+type DdnsWhitelist struct {
+	next http.Handler
+	name string
 }
 
-// New created a new Demo plugin.
+// New created a new DDNSwhitelist plugin
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.Headers) == 0 {
-		return nil, errors.New("headers cannot be empty")
-	}
-
-	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+	return &DdnsWhitelist{
+		next: next,
+		name: name,
 	}, nil
 }
 
-func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
-	}
-
+// ServeHTTP DDNSwhitelist
+func (a *DdnsWhitelist) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	a.next.ServeHTTP(rw, req)
 }
