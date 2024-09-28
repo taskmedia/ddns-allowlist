82084bd4fa30bbc47ab1cf0cb2d6d6c9b3e90f70
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2024-09-28 14:40:19 +0200

    chore: basic functionality

--- dyndnswhitelist.go
+++ dyndnswhitelist.go
@@ -3,13 +3,26 @@ package ddnswhitelist
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
+)
+
+const (
+	xForwardedFor = "X-Forwarded-For"
 )
 
 // Config the plugin configuration
 type Config struct {
 	DdnsHostList []string `json:"ddnsHostList,omitempty"` // Add hosts to whitelist
 }
+
+type AllowedIps []*net.IP
 
 // CreateConfig creates the default plugin configuration
 func CreateConfig() *Config {
@@ -20,19 +33,123 @@ func CreateConfig() *Config {
 
 // DDNSwhitelist plugin
 type DdnsWhitelist struct {
-	next http.Handler
-	name string
+	config *Config
+	name   string
+	next   http.Handler
 }
 
 // New created a new DDNSwhitelist plugin
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.DdnsHostList) == 0 {
+		return nil, errors.New("no host list provided")
+	}
+
+	for _, host := range config.DdnsHostList {
+		if !govalidator.IsDNSName(host) {
+			return nil, fmt.Errorf("invalid host provided: %v", host)
+		}
+	}
+
 	return &DdnsWhitelist{
-		next: next,
-		name: name,
+		name:   name,
+		next:   next,
+		config: config,
 	}, nil
 }
 
 // ServeHTTP DDNSwhitelist
 func (a *DdnsWhitelist) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// TODO: this might be scheduled and not requested on every request
+	// get list of allowed IPs
+	aIps, err := NewAllowedIps(a.config.DdnsHostList)
+	if err != nil {
+		log.Printf("DDNSwhitelist: could not look up ip address: %v", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	reqIpAddr := a.GetRemoteIP(req)
+	reqIpAddrLenOffset := len(reqIpAddr) - 1
+
+	for i := reqIpAddrLenOffset; i >= 0; i-- {
+		isAllowed, err := aIps.Contains(reqIpAddr[i])
+		if err != nil {
+			log.Printf("%v", err)
+		}
+
+		if !isAllowed {
+			log.Printf("DDNSwhitelist: request denied [%s]", reqIpAddr[i])
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+	}
+
 	a.next.ServeHTTP(rw, req)
+}
+
+func (a *AllowedIps) Contains(ipString string) (bool, error) {
+	if len(ipString) == 0 {
+		return false, errors.New("empty IP address")
+	}
+
+	ipAddr := net.ParseIP(ipString)
+	if ipAddr == nil {
+		return false, fmt.Errorf("unable to parse IP address: %s", ipString)
+	}
+
+	for _, ip := range *a {
+		if ip.Equal(ipAddr) {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// GetRemoteIP returns a list of IPs that are associated with this request
+// from https://github.com/kevtainer/denyip/blob/28930e800ff2b37b692c80d72c883cfde00bde1f/denyip.go#L76-L105
+func (a *DdnsWhitelist) GetRemoteIP(req *http.Request) []string {
+	var ipList []string
+
+	xff := req.Header.Get(xForwardedFor)
+	xffs := strings.Split(xff, ",")
+
+	for i := len(xffs) - 1; i >= 0; i-- {
+		xffsTrim := strings.TrimSpace(xffs[i])
+
+		if len(xffsTrim) > 0 {
+			ipList = append(ipList, xffsTrim)
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		remoteAddrTrim := strings.TrimSpace(req.RemoteAddr)
+		if len(remoteAddrTrim) > 0 {
+			ipList = append(ipList, remoteAddrTrim)
+		}
+	} else {
+		ipTrim := strings.TrimSpace(ip)
+		if len(ipTrim) > 0 {
+			ipList = append(ipList, ipTrim)
+		}
+	}
+
+	return ipList
+}
+
+func NewAllowedIps(hosts []string) (*AllowedIps, error) {
+	aIps := &AllowedIps{}
+
+	for _, host := range hosts {
+		ip, err := net.LookupIP(host)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, i := range ip {
+			*aIps = append(*aIps, &i)
+		}
+	}
+
+	return aIps, nil
 }
--- dyndnswhitelist_test.go
+++ dyndnswhitelist_test.go
@@ -5,43 +5,126 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestDemo(t *testing.T) {
-	cfg := CreateConfig()
-	cfg.Headers["X-Host"] = "[[.Host]]"
-	cfg.Headers["X-Method"] = "[[.Method]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-Demo"] = "test"
+func TestDdnsWhitelist(t *testing.T) {
 
-	ctx := context.Background()
-	next := http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {})
-
-	handler, err := New(ctx, next, cfg, "demo-plugin")
-	if err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		desc          string
+		ddnsList      []string
+		expectedError bool
+	}{
+		{
+			desc:          "empty host list",
+			ddnsList:      []string{},
+			expectedError: true,
+		},
+		{
+			desc:          "invalid host",
+			ddnsList:      []string{"foo", "bar!fo"},
+			expectedError: true,
+		},
+		{
+			desc:     "valid host - localhost",
+			ddnsList: []string{"localhost"},
+		},
+		{
+			desc:     "valid host - github.com",
+			ddnsList: []string{"github.com"},
+		},
 	}
 
-	recorder := httptest.NewRecorder()
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
-	if err != nil {
-		t.Fatal(err)
+			cfg := CreateConfig()
+			cfg.DdnsHostList = test.ddnsList
+
+			ctx := context.Background()
+			next := http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {})
+
+			handler, err := New(ctx, next, cfg, "ddnswhitelist-plugin")
+
+			if test.expectedError {
+				assert.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.NotNil(t, handler)
+			}
+		})
 	}
-
-	handler.ServeHTTP(recorder, req)
-
-	assertHeader(t, req, "X-Host", "localhost")
-	assertHeader(t, req, "X-URL", "http://localhost")
-	assertHeader(t, req, "X-Method", "GET")
-	assertHeader(t, req, "X-Demo", "test")
 }
 
-func assertHeader(t *testing.T, req *http.Request, key, expected string) {
-	t.Helper()
+func TestDdnsWhitelist_ServeHTTP(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		ddnsList  []string
+		reqIPAddr string
+		expected  int
+	}{
+		{
+			desc:      "allowed host internal - localhost",
+			ddnsList:  []string{"localhost"},
+			reqIPAddr: "127.0.0.1",
+			expected:  http.StatusOK,
+		},
+		{
+			desc:      "allowed host external - dns.google",
+			ddnsList:  []string{"dns.google"},
+			reqIPAddr: "8.8.8.8",
+			expected:  http.StatusOK,
+		},
+		{
+			desc:      "denied host internal - localhost",
+			ddnsList:  []string{"localhost"},
+			reqIPAddr: "10.10.10.10",
+			expected:  http.StatusForbidden,
+		},
+		{
+			desc:      "denied host external - dns.google",
+			ddnsList:  []string{"localhost"},
+			reqIPAddr: "8.8.8.8",
+			expected:  http.StatusForbidden,
+		},
+		{
+			desc:      "invalid host list",
+			ddnsList:  []string{"invalid"},
+			reqIPAddr: "127.0.0.1",
+			expected:  http.StatusInternalServerError,
+		},
+	}
 
-	if req.Header.Get(key) != expected {
-		t.Errorf("invalid header value: %s", req.Header.Get(key))
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			cfg := CreateConfig()
+			cfg.DdnsHostList = test.ddnsList
+
+			ctx := context.Background()
+
+			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			})
+
+			handler, err := New(ctx, next, cfg, "ddnswhitelist-plugin")
+			require.NoError(t, err)
+			assert.NotNil(t, handler)
+
+			req := &http.Request{
+				RemoteAddr: test.reqIPAddr,
+			}
+
+			rec := httptest.NewRecorder()
+			handler.ServeHTTP(rec, req)
+
+			assert.Equal(t, test.expected, rec.Code)
+		})
 	}
 }
