3a57d0ef7cebaadc4cf76792d34aaf50fe888408
Marco Lecheler <marco@task.media>
2024-10-08 13:06:54 +0200

    chore(test): add basic New() test

--- ddnsallowlist_test.go
+++ ddnsallowlist_test.go
@@ -5,11 +5,77 @@
 package ddns_allowlist
 
 import (
+	"fmt"
 	"sort"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCreateConfig(t *testing.T) {
+	t.Run("create config", func(t *testing.T) {
+		config := CreateConfig()
+		assert.NotNil(t, config)
+	})
+}
+
+func TestNew(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		config *DdnsAllowListConfig
+		err    error
+	}{
+		{
+			desc:   "empty sourceRangeHosts",
+			config: &DdnsAllowListConfig{},
+			err:    errEmptySourceRangeHosts,
+		},
+		{
+			desc: "modified rejectStatus",
+			config: &DdnsAllowListConfig{
+				SourceRangeHosts: []string{"example.com"},
+				RejectStatusCode: 200,
+			},
+		},
+		{
+			desc: "invalid rejectStatus",
+			config: &DdnsAllowListConfig{
+				SourceRangeHosts: []string{"example.com"},
+				RejectStatusCode: 999,
+			},
+			err: fmt.Errorf("%w: %d", errInvalidHTTPStatuscode, 999),
+		},
+		{
+			desc: "lookupInterval",
+			config: &DdnsAllowListConfig{
+				SourceRangeHosts: []string{"example.com"},
+				LookupInterval:   10,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		// t.Parallel()
+		t.Run(tc.desc, func(t *testing.T) {
+			dal, err := New(nil, nil, tc.config, "test")
+
+			if err != nil {
+				assert.Equal(t, tc.err, err)
+				return
+			}
+
+			if tc.config.RejectStatusCode != 0 {
+				assert.Equal(t, tc.config.RejectStatusCode, dal.(*ddnsAllowLister).rejectStatusCode)
+			}
+
+			if tc.config.LookupInterval != 0 {
+				expectedInterval := time.Duration(tc.config.LookupInterval) * time.Second
+				assert.Equal(t, expectedInterval, dal.(*ddnsAllowLister).lookupInterval)
+			}
+		})
+	}
+}
 
 func TestResolveHosts(t *testing.T) {
 	testCases := []struct {
