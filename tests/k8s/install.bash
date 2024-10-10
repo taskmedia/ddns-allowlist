f58c9d6ef2dfd5a541cca1e45fb687263b3da03b
Marco Lecheler <marco@task.media>
2024-10-10 11:21:31 +0200

    chore: test date 10s

--- tests/k8s/install.bash
+++ tests/k8s/install.bash
@@ -41,10 +41,10 @@ helm upgrade --install traefik \
 
 # deploy demo application
 ensure_namespace whoami
-time
+date
 kubectl apply -f "${DIR_TESTS_K8s}/resources/service.yml" --namespace whoami
-sleep 5
-time
+sleep 10
+date
 kubectl apply -f "${DIR_TESTS_K8s}/resources/allow.yml" --namespace whoami
 kubectl apply -f "${DIR_TESTS_K8s}/resources/deny.yml" --namespace whoami
 
